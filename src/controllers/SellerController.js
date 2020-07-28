68018e987230ba65720f2de6f2dd1902cc9bf604
Júlio Cesar Gomes <julio_gomes92@hotmail.com>
2020-07-28 20:50:27 -0300

    put e delete do seller adicionado

--- src/controllers/SellerController.js
+++ src/controllers/SellerController.js
@@ -4,8 +4,25 @@ const Seller = require('../db/schemas/Seller');
 const sellerRouter = express.Router()
 
 
-sellerRouter.get('/', (request, response) => {
-    response.json({ message: 'acessou' })
+sellerRouter.get('/', async (request, response) => {
+    var sellers = await Seller.find();
+
+    if (sellers.length > 0) {
+        response.status(200).json(sellers)
+    } else {
+        response.status(404).json({message: "Nenhum dado encontrado"})
+    }
+})
+
+sellerRouter.get('/:username', async (request, response) => {
+    var username = request.params.username;
+    var seller = await Seller.findOne({"user.username": username});
+
+    if (seller != null && seller != undefined) {
+        response.status(200).json(seller)
+    } else {
+        response.status(404).json({ message: "Nenhum dado encontrado" })
+    }
 })
 
 sellerRouter.post('/', async (request, response) => {
@@ -26,4 +43,35 @@ sellerRouter.post('/', async (request, response) => {
 
 })
 
-module.exports = sellerRouter; 
+sellerRouter.put('/:id', async (request, response) => {
+    const sellerBody = request.body;
+    const id = request.params.id;
+
+    if (sellerBody && Object.keys(sellerBody).length > 0) {
+       var documentUpdated =  await seller.updateOne({_id: id},sellerBody)
+            if (documentUpdated.nModified > 0) {
+                response.status(404).json({ message: "O documento foi atualizado com sucesso" })
+        } response.status(500).json({ message: "Não foi possível atualizar" })
+    } else {
+        response.status(400).json({ message: "Faltou o body" })
+    }
+
+})
+
+sellerRouter.delete('/:id', async (request, response) => {
+    const id = request.params.id;
+    var documentDeleted = await seller.deleteOne({ _id: id }, sellerBody)
+
+    if (sellerBody && Object.keys(sellerBody).length > 0) {
+        var documentUpdated = await seller.updateOne({ _id: id }, sellerBody)
+        if (documentUpdated.nModified > 0) {
+            response.status(404).json({ message: "O documento foi atualizado com sucesso" })
+        } response.status(500).json({ message: "Não foi possível atualizar" })
+    } else {
+        response.status(400).json({ message: "Faltou o body" })
+    }
+
+})
+
+
+module.exports = sellerRouter;
--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://127.0.0.1:27017/sge", () => {
-    useNewUrlParser: true; useUnifiedTopology: true
+  useNewUrlParser: true
     });
 
 const db = mongoose.connection;
